a209b5759eb0658d6aa853ca8a5662ca63964458
Tshambetaliev <tshambetaliev@gmail.com>
2023-02-21 00:15:30 +0600

    Initial commit

---
+++ app/src/main/java/com/example/tilek_shambetaliev_hw3_4/ContinentFragment.java
@@ -0,0 +1,61 @@
+package com.example.tilek_shambetaliev_hw3_4;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tilek_shambetaliev_hw3_4.databinding.FragmentContinentBinding;
+
+import java.util.ArrayList;
+
+public class ContinentFragment extends Fragment implements OnClick {
+
+    private FragmentContinentBinding binding;
+
+    private ArrayList<String> continentList;
+    private ContinentAdapter continentAdapter;
+    private Integer posit;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentContinentBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        loadData();
+        continentAdapter = new ContinentAdapter(continentList, this);
+        binding.rvContinents.setAdapter(continentAdapter);
+    }
+
+    private void loadData() {
+        continentList = new ArrayList<>();
+        continentList.add("Asia");
+        continentList.add("Africa");
+        continentList.add("Americas");
+        continentList.add("Europa");
+    }
+
+    @Override
+    public void onClick(int post) {
+        Bundle bundle = new Bundle();
+        bundle.putInt("POSIT", post);
+        CountryFragment fragment = new CountryFragment();
+        fragment.setArguments(bundle);
+        requireActivity().getSupportFragmentManager().beginTransaction().
+                replace(R.id.container, fragment).addToBackStack(null).commit();
+
+    }
+}
---
+++ app/src/main/java/com/example/tilek_shambetaliev_hw3_4/OnClick.java
@@ -0,0 +1,7 @@
+package com.example.tilek_shambetaliev_hw3_4;
+
+public interface OnClick {
+
+    void onClick(int position);
+
+}
